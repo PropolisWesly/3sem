dbf1602f2ea8d262ef22c25be2b3ec0c20664322
PropolisWesly <sashaxmares@gmail.com>
2017-10-07 13:03:41 +0300

    redone task1

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -15,13 +15,16 @@
 #include <ctype.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main (int argc, char *argv[], char *envp[])
 {
 
 	pid_t pid=fork();
-	int status;
-	char str[100];
+	int status, i;
+	char s[256];
+	char *str=(char *)malloc(256*sizeof(char));
+	char **args=(char**)malloc(256*sizeof(char*));
 	
 	if (pid<0)
 	{
@@ -33,7 +36,7 @@ int main (int argc, char *argv[], char *envp[])
 	{
 		waitpid(pid, &status, 0);
 		printf("Ret code: %d\n", WEXITSTATUS(status));
-		if (WEXITSTATUS(status)==0)
+		if (WEXITSTATUS(status)==42)
 			exit(0);
 		else pid = fork();
 
@@ -45,12 +48,24 @@ int main (int argc, char *argv[], char *envp[])
 
 		
 	}
-	scanf("%s", str);
+	gets(str, 255, stdin);
+
 	if ((str[0]=='q')&&(str[1]=='\0'))
-		exit(0);
-	execvp(str, str);
+		exit(42);
+	
+	for(i=0; strlen(str)>0; i++)
+	{
+		sscanf(str, "%s", s);
+		args[i]=(char*)malloc(256*sizeof(char));
+		strcpy(args[i], s);
+		str= str+ strlen(s)+1;
+		
+		
+	}
+
+	execvp(args[0], &args[0]);
 	printf("Can't open program\n");
-	exit(0);
+	exit(42);
 	
 	
 	return 0;
